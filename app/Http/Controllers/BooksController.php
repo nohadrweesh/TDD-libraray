09a30d0f62f9cc8e84bb4b58c6a871e9ebd53a4e
nohadrweesh <noha.drweesh@robovics.com>
2021-02-23 20:46:13 +0200

    add book add,update api,tests for them

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    //
+
+    public function store(){
+        $data=request()->validate([
+            'title'=>'required|string',
+            'author'=>'required|string'
+        ]);
+        Book::create($data);
+
+    }
+
+    public function update(Book $book){
+        $data=request()->validate([
+            'title'=>'required|string',
+            'author'=>'required|string'
+        ]);
+        $book->update($data);
+
+    }
+}
---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Book;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_book_can_be_added_to_the_library(){
+
+        $this->withoutExceptionHandling();
+        $response=$this->post('/api/books',[
+            'title'=>'New Book',
+            'author'=>'Noha Drweesh'
+        ]);
+        $response->assertOk();
+        $this->assertCount(1,Book::all());
+    }
+
+    /** @test */
+    public function a_author_is_required(){
+        //$this->withoutExceptionHandling(); to view error itself
+        $response=$this->post('/api/books',[
+            'title'=>'New Book',
+            'author'=>''
+        ]);
+        $response->assertSessionHasErrors('author');
+
+    }
+
+    /** @test */
+    public function a_book_can_be_updated(){
+
+        $this->withoutExceptionHandling();
+        $this->post('/api/books',[
+            'title'=>'New Book',
+            'author'=>'Noha Drweesh'
+        ]);
+        $book=Book::first();
+        $this->patch('/api/books/'.$book->id,[
+            'title'=>'New Book updated',
+            'author'=>'Noha Darweesh'
+        ]);
+
+        $this->assertEquals('New Book updated',Book::first()->title);
+        $this->assertEquals('Noha Darweesh',Book::first()->author);
+    }
+}
